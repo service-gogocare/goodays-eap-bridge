5fe92fca398c8946530a099dd752702be3851746
mark5227cc <mark5227cc@gmail.com>
2026-07-24 13:32:26 +0800

    新增圖片壓縮功能以縮短上傳時間，調整審核密碼輸入欄位為密碼類型，並新增按 Enter 鍵確認的功能

--- assets/config.js
+++ assets/config.js
@@ -7,5 +7,5 @@ window.APP_CONFIG = {
   WEBAPP_URL: 'https://script.google.com/macros/s/AKfycbxBFnYCBDnXS40IlJRzKQiCYksd6bZvdtL3LJqUBlqUoXIBAvS5yaXdXv4497VavAh5/exec',
 
   // 除錯開關：true 時後端會把真正的錯誤訊息回傳到畫面（僅供排錯，正式上線請改 false）
-  DEBUG: true
+  DEBUG: false
 };
